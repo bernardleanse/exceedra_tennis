6cf810434247b821883aa5010140bfdfed09cb84
Bernard Leanse <bernardleanse96@gmail.com>
2022-03-14 17:24:47 +0000

    get and assign rank names

--- classes/PlayerListManipulation.js
+++ classes/PlayerListManipulation.js
@@ -1,12 +1,13 @@
 class PlayerListManipulation {
+ 
   static sortData(data) {
-    const rankedData = this.filterOutUnranked(data)
-    const orderedRanked = this.assignCurrentPosition(rankedData)
-    const unRankedData = this.filterOutRanked(data)
-    const unOrderedRanked = this.assignCurrentPosition(unRankedData)
-    return orderedRanked.concat(unOrderedRanked)
+    const assignRankNameData = this.assignRankNames(data)
+    const positionedData = this.assignCurrentPosition(assignRankNameData)
+    const ranked = this.filterOutUnranked(positionedData)
+    const unranked = this.filterOutRanked(positionedData)
+    return ranked.concat(unranked)
   }
-  
+
   static assignCurrentPosition(data){
     let currentPosition = 1
     
@@ -26,6 +27,31 @@ class PlayerListManipulation {
     const ranked = data.filter((obj) => obj.rankName == null)
     return ranked
   }
+
+  static getRankName(points){
+    if (points < 3000) {
+      return "Bronze"
+    } else if (points < 5000 ) {
+      return "Silver"
+    } else if (points < 10000) {
+      return "Gold"
+    } else {
+      return "Supersonic Legend"
+    }
+  }
+
+  static assignRankNames(data) {
+    return data.map(player => {
+      const rankName = this.getRankName(player.points)
+      if (player.matchesPlayed < 3)
+        return player
+      else {
+        return { ...player, rankName }
+      }
+    })
+  }
 }
+
+
 
 module.exports = PlayerListManipulation
